332fa4f329aa489f6d49c15792a1d68137fd7b8e
Matt Crider <mattcrider@gmail.com>
2015-08-06 14:48:21 -0400

    Handle directory (tag) name changes

--- server/server.js
+++ server/server.js
@@ -13,26 +13,28 @@ Meteor.startup(function () {
 
   watcher
     .on('add', Meteor.bindEnvironment(function (path) {
-        var exploded = path.split('/');
-        var imagePath = exploded.slice(subdirectories, exploded.length).join('/');
-        var categories = exploded.slice(subdirectories, exploded.length - 1);
-        var filename = exploded[exploded.length - 1];
+      var exploded = path.split('/');
+      var imagePath = exploded.slice(subdirectories, exploded.length).join('/');
+      var categories = exploded.slice(subdirectories, exploded.length - 1);
+      var filename = exploded[exploded.length - 1];
 
-        var image = Images.findOne({ path: imagePath });
-        if (image) {
-          return false;
-        }
+      var image = Images.findOne({ path: imagePath });
+      if (image) {
+        return false;
+      }
 
-        Images.insert({
-          path: imagePath,
-          filename: filename,
-          categories: categories,
-          date: new Date()
-        });
+      Images.insert({
+        path: imagePath,
+        filename: filename,
+        categories: categories,
+        date: new Date()
+      });
     }))
-    .on('change', function (path) { console.log(path, 'has been changed'); })
+    .on('change', function (path) { console.log(path + ' has been changed'); })
     .on('unlink', Meteor.bindEnvironment(function (path) {
-      var image = Images.findOne({ path: path });
+      var exploded = path.split('/');
+      var imagePath = exploded.slice(subdirectories, exploded.length).join('/');
+      var image = Images.findOne({ path: imagePath });
       if (!image) {
         return false;
       }
